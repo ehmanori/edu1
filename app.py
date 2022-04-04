6add8c2d2a6962074ce277d5d5c427145754a7f3
ehmanori <47213026+ehmanori@users.noreply.github.com>
2022-04-04 11:59:35 +0900

    Revert "Update app.py"

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! tag 설정"
+    return "Hello World! webhook 적용"
   
 if __name__ == '__main__':
     app.run(host= '0.0.0.0',port=8080,debug=True)
